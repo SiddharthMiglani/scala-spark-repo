725b42a1f501007e98cc7d7c9d4b6bdef533ac1b
Siddharth <siddharthmiglani@gmail.com>
2018-02-07 19:50:50 +0530

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "scala-spark-repo"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+    
---
+++ src/main/scala/sampleObject.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by siddharthmi on 2/7/2018.
+  */
+object sampleObject {
+  def main(args: Array[String]): Unit = {
+    print("Hello World");
+  }
+}
