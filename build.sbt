51db55b206dd03a2667a9d938eb6602cf1996e30
Siddharth <siddharthmiglani@gmail.com>
2018-03-22 19:05:19 +0530

    Upgraded scala's hello world to sparks's hello world i.e. word count

--- build.sbt
+++ build.sbt
@@ -3,4 +3,9 @@ name := "scala-spark-repo"
 version := "1.0"
 
 scalaVersion := "2.11.8"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-core
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.0"
+
+
     
--- src/main/scala/sampleObject.scala
+++ src/main/scala/sampleObject.scala
@@ -1,8 +1,36 @@
 /**
   * Created by siddharthmi on 2/7/2018.
   */
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
 object sampleObject {
   def main(args: Array[String]): Unit = {
-    print("Hello World");
+    //Create conf object
+    val conf = new SparkConf().setAppName("WordCount")
+
+    //create spark context object
+    val sc = new SparkContext(conf)
+
+    //Check whether sufficient params are supplied
+    if (args.length < 2) {
+      println("Usage: ScalaWordCount <input> <output>")
+      System.exit(1)
+    }
+
+    //Read file and create RDD
+    val rawData = sc.textFile(args(0))
+
+    //convert the lines into words using flatMap operation
+    val words = rawData.flatMap(line => line.split("\t"))
+
+    //count the individual words using map and reduceByKey operation
+    val wordCount = words.map(word => (word, 1)).reduceByKey(_ + _)
+
+    //Save the result
+    wordCount.saveAsTextFile(args(1))
+
+    //stop the spark context
+    sc.stop
   }
 }
